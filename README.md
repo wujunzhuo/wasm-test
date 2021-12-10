4ff8dbc666782b9a0959f45b1a0f990cd2e4c8be
Junzhuo Wu <wujz@cloudbrain.ai>
2021-12-10 17:41:57 +0800

    refactor: run

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ```sh
 $ task all
     
-$ ./run/run example/go/sum/sum.wasm
+$ ./run example/go/sum/sum.wasm
     
-$ ./run/run example/rust/sum/pkg/sum_bg.wasm
+$ ./run example/rust/sum/pkg/sum_bg.wasm
 ```
--- run.go
+++ run.go
@@ -7,6 +7,16 @@ import (
 	"github.com/second-state/WasmEdge-go/wasmedge"
 )
 
+func runSum(vm *wasmedge.VM, a int32, b int32) {
+	fmt.Println(" Running sum: ", a, b)
+	var ret, err = vm.ExecuteBindgen("sum", wasmedge.Bindgen_return_i32, a, b)
+	if err == nil {
+		fmt.Println(" Return value: ", ret.(int32))
+	} else {
+		fmt.Println(" !!! Error: ", err.Error())
+	}
+}
+
 func main() {
 	wasmedge.SetLogErrorLevel()
 	var conf = wasmedge.NewConfigure(wasmedge.WASI)
@@ -15,20 +25,12 @@ func main() {
 	var wasi = vm.GetImportObject(wasmedge.WASI)
 	wasi.InitWasi([]string{}, os.Environ(), []string{".:."})
 
-	fmt.Println(" ### Loading wasm file: ", os.Args[1])
+	fmt.Println(" Loading wasm file: ", os.Args[1])
 	vm.LoadWasmFile(os.Args[1])
 	vm.Validate()
 	vm.Instantiate()
 
-	var a, b int32 = 23, 16
-
-	fmt.Println(" ### Running sum: ", a, b)
-	var ret, err = vm.ExecuteBindgen("sum", wasmedge.Bindgen_return_i32, a, b)
-	if err == nil {
-		fmt.Println(" Return value: ", ret.(int32))
-	} else {
-		fmt.Println(" !!! Error: ", err.Error())
-	}
+	runSum(vm, 23, 16)
 
 	vm.Release()
 	conf.Release()
